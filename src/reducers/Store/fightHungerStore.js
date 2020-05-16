7eabff51d462ed55ea1618592a78c0128a132834
Jordan Kagmeni <kagmeni77@gmail.com>
2020-05-16 05:59:17 +0200

    auth: define initial store

---
+++ src/reducers/Store/fightHungerStore.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/prefer-default-export */
+/**
+ * @constant initialStore
+ * @description initial store of App
+ */
+
+export const initialStore = {
+  User: [],
+  Domain: [],
+  isAuth: false
+};
