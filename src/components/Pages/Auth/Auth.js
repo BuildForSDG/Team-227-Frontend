202bd59daa29999adebff12b29e401ea6f7504af
Jordan Kagmeni <kagmeni77@gmail.com>
2020-05-17 12:14:27 +0200

    auth: update Auth Page Define the propTypes, props and pass it in the children components #

--- src/components/Pages/Auth/Auth.js
+++ src/components/Pages/Auth/Auth.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../../Header/Header.jsx';
 import Login from '../../Login/Login.jsx';
 
-function Auth() {
-  const isAuth = Math.floor(Math.random() * 2) !== 0;
+function Auth({
+  isAuth, loading, error, onSubmitForm
+}) {
   return (
     <div>
-    <Header isAuth={isAuth} />
-      <Login />
+      <Header isAuth={isAuth} />
+      <Login isAuth={isAuth} loading={loading} error={error} onSubmitForm={onSubmitForm} />
     </div>
   );
 }
+
+Auth.propTypes = {
+  isAuth: PropTypes.bool.isRequired,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.object.isRequired,
+  onSubmitForm: PropTypes.func.isRequired
+};
 
 export default Auth;
