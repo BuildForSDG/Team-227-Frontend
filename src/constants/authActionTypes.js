d930075ae0ce6c472767e0714107c7e3e1299100
Jordan Kagmeni <kagmeni77@gmail.com>
2020-05-17 12:23:17 +0200

    auth: Add action types add actions types of success auth and failed auth #

--- src/constants/authActionTypes.js
+++ src/constants/authActionTypes.js
@@ -1,7 +1,19 @@
 /* eslint-disable import/prefer-default-export */
 /**
- * Auth Constants
+ * Auth Request
  * @constant {String} SIGN_IN
  */
 
 export const SIGN_IN = 'SIGN_IN';
+
+/**
+ * Auth Request Success
+ * @constant {String} SIGN_IN_SUCCESS
+ */
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
+
+/**
+ * Auth Request Error
+ * @constant {String} SIGN_IN_ERROR
+ */
+export const SIGN_IN_ERROR = 'SIGN_IN_ERROR';
