66b90d128cdcdd384dfe8c1e73475e9b2c7bb6eb
Jordan Kagmeni <kagmeni77@gmail.com>
2020-05-18 17:29:17 +0200

    auth: remove file

--- src/components/Pages/Auth/Auth.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Auth from './Auth';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Auth />);
-  const element = getByText(/Fight Hunger/);
-  expect(element).toBeInTheDocument();
-});
