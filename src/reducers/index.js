13f014d8857856018cf11759a0d16417aa7d8bba
Jordan Kagmeni <kagmeni77@gmail.com>
2020-05-29 03:02:19 +0200

    register: add reducer Add reducer of register feature in combineReducers #

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import registerReducer from './registerReducer';
 
 /**
  * @constant rootReducers
  * @description Combine all reducers of app
  */
-const rootReducers = combineReducers({ authReducer });
+const rootReducers = combineReducers({ authReducer, registerReducer });
 
 export default rootReducers;
